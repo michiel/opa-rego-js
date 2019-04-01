2494c8cf3339933fbcd0e7ef5c70743b6d2cb49d
Michiel Kalkman <michiel@nosuchtype.com>
2019-04-01 15:41:05 +1100

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,38 @@
 export const Greeter = (name: string) => `Hello ${name}`;
+
+const request = require('request');
+
+enum HttpMethod {
+  GET,
+    POST,
+    PUT,
+    DELETE,
+}
+
+class OPAResult {
+  constructor(obj: any) {
+  }
+}
+
+function queryOPA(method:HttpMethod, path: string[], ...args:any[]) : OPAResult {
+  const input = {
+    method: method,
+    path: path,
+    subject: 'asd',
+    ...args
+  };
+
+  return compile('', input, []);
+
+}
+
+function compile(query:string, input: any, unknowns: string[]) : OPAResult {
+  const response = request({
+    uri: 'http://localhost:8181/v1/compile',
+    method: 'POST',
+    json: true,
+    body: { query, input, unknowns });
+
+  return new OPAResult({});
+}
+
